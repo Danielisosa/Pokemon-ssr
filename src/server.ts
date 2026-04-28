51b6032cf7138926c5328b24618b284ae7a174c6
danieli sosa <danielisosa.0512@gmail.com>
2026-04-28 10:13:50 -0400

    update main.server.ts y server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,77 +1,13 @@
 import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
-import {
-  AngularNodeAppEngine,
-  createNodeRequestHandler,
-  isMainModule,
-  writeResponseToNodeResponse,
-} from '@angular/ssr/node';
 import { getContext } from '@netlify/angular-runtime/context.mjs';
-import express from 'express';
-import { join } from 'node:path';
 
-const browserDistFolder = join(import.meta.dirname, '../browser');
-
-const angularAppEngine = new AngularAppEngine({
-  bootstrap: () => import('./main.server').then(m => m.default),
-});
+const angularAppEngine = new AngularAppEngine();
 
 export async function netlifyAppEngineHandler(request: Request): Promise<Response> {
   const context = getContext();
-  const result = await angularAppEngine.render(request, context);
-  return result.response;
+
+  const result = await angularAppEngine.handle(request, context);
+  return result || new Response('Not found', { status: 404 });
 }
 
 export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
-
-/**
- * Example Express Rest API endpoints can be defined here.
- * Uncomment and define endpoints as necessary.
- *
- * Example:
- * ```ts
- * app.get('/api/{*splat}', (req, res) => {
- *   // Handle API request
- * });
- * ```
- */
-
-const app = express();
-const nodeAngularApp = new AngularNodeAppEngine();
-
-/**
- * Serve static files from /browser
- */
-app.use(
-  express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: false,
-    redirect: false,
-  }),
-);
-
-/**
- * Handle all other requests by rendering the Angular application.
- */
-app.use((req, res, next) => {
-  nodeAngularApp
-    .handle(req)
-    .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
-    .catch(next);
-});
-
-/**
- * Start the server if this module is the main entry point, or it is ran via PM2.
- * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
- */
-if (isMainModule(import.meta.url) || process.env['pm_id']) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
-    if (error) {
-      throw error;
-    }
-
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
-}
-
-export const nodeReqHandler = createNodeRequestHandler(app);
